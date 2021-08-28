c8f1fcd9299ba5ce635bb1fa12e44b36e7b4dc04
LeeSeungHun <58622039+superstarMann@users.noreply.github.com>
2021-08-28 16:05:41 +0900

    setting typeOrm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,39 @@
 import { Module } from '@nestjs/common';
+import * as Joi from 'joi';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { StoresModule } from './stores/stores.module';
 
 @Module({
-  imports: [StoresModule, GraphQLModule.forRoot({
-    autoSchemaFile: true})],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === "dev" ? ".env.dev" : ".test.env",
+      ignoreEnvFile: process.env.NODE_ENV === 'prod',
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid('dev', 'prod').required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT:Joi.string().required(),
+        DB_USERNAME:Joi.string().required(),
+        DB_PASSWORD:Joi.string().required(),
+        DB_DATABASE:Joi.string().required(),
+      })
+    }),
+    TypeOrmModule.forRoot({
+      "type": "postgres",
+      "host": process.env.DB_HOST,
+      "port": +process.env.DB_PORT,
+      "username": process.env.DB_USERNAME,
+      "password": process.env.DB_PASSWORD,
+      "database": process.env.DB_DATABASE,
+      "synchronize": true,
+      "logging": true
+    }),
+    GraphQLModule.forRoot({
+    autoSchemaFile: true}),
+    StoresModule,
+  ],
   controllers: [],
   providers: [],
 })
